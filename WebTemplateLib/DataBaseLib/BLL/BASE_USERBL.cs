677746dc20adef4410aa4b17798994a86fcdc0a5
Jerry.si <403563310@qq.com>
2019-01-20 22:45:07 +0800

    增加用户权限表

---
+++ WebTemplateLib/DataBaseLib/BLL/BASE_USERBL.cs
@@ -0,0 +1,75 @@
+﻿using DataBaseLib.DAL;
+using DataBaseLib.Model;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace DataBaseLib.BLL {
+    public partial interface IBASE_USERBL :
+        DataBaseLib.Infrastructure.BLL.IGenericBL<BASE_USER> {
+        IQueryable<BASE_USER> QueryAll();
+        BASE_USER GetByAccount(string name, string pwd);
+    }
+
+    public partial class BASE_USERBL : 
+       DataBaseLib.Infrastructure.BLL.GenericBL<BASE_USER>,
+       IBASE_USERBL {
+
+        
+        protected const string kCacheKeyPrefix = "_bl_BASE_USERBL";
+
+        public IQueryable<BASE_USER> QueryAll() {
+            return Repository.Get();
+        }
+
+        public BASE_USER GetByAccount(string name, string pwd) {
+            return Repository.Get(i => i.UserAccount == name && i.UserPWD == pwd).FirstOrDefault();
+        }
+        /// <summary>
+        /// ///////////////////////////////////////////
+        /// </summary>
+
+        #region 统一
+
+        public BASE_USERBL() : base() {
+            unitOfWork = new DataBaseLib.DAL.UnitOfWork();
+            OnCreate();
+        }
+        public BASE_USERBL(DataBaseLib.DAL.UnitOfWork unitOfWork) : base() {
+            this.unitOfWork = unitOfWork;
+            OnCreate();
+        }
+        partial void OnCreate();
+        private DataBaseLib.DAL.UnitOfWork UnitOfWork {
+            get { return unitOfWork as DataBaseLib.DAL.UnitOfWork; }
+            set { unitOfWork = value; }
+        }
+        private IBASE_USER_dal _repository;
+        private IBASE_USER_dal Repository {
+            get {
+                if (_repository == null) {
+                    _repository = unitOfWork.Repository<BASE_USER>() as IBASE_USER_dal;
+                }
+                return _repository;
+            }
+            set {
+                _repository = value;
+            }
+        }
+
+        public override BASE_USER InsertOrUpdate(BASE_USER entity) {
+            if (entity.ID == 0) {
+                return Insert(entity);
+            }
+            Update(entity);
+            return entity;
+        }
+
+        #endregion
+
+    }
+
+
+
+
+}
---
+++ WebTemplateLib/DataBaseLib/DAL/BASE_USER_dal.cs
@@ -0,0 +1,53 @@
+﻿using DataBaseLib.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity.Core.EntityClient;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseLib.DAL {
+
+    public partial interface IBASE_USER_dal :
+    DataBaseLib.Infrastructure.DAL.IRepository<BASE_USER> {
+
+    }
+
+
+    public class BASE_USER_dal :
+       DataBaseLib.Infrastructure.DAL.GenericRepository<BASE_USER>,
+       DataBaseLib.DAL.IBASE_USER_dal {
+
+        private DataBaseLib.Model.ManagerSiContext context { get { return this.UnitOfWork.Context; } }
+        private DataBaseLib.DAL.UnitOfWork UnitOfWork { get; set; }
+        public BASE_USER_dal(DAL.UnitOfWork unitOfWork) : base (unitOfWork)
+      {
+            this.UnitOfWork = unitOfWork;
+        }
+
+
+
+
+        /// <summary>
+        /// LINQ Method syntax
+        /// </summary>
+        /// <returns></returns>
+        public BASE_USER GetByAccount(string name,string pwd) {             
+            using(var context = new ManagerSiContext()) {
+                var list = context.BASE_USER.Where(i => i.State == "1" && i.UserAccount == name && i.UserPWD == pwd).FirstOrDefault();
+                return list;
+            }
+        }
+        /// <summary>
+        /// LINQ Query syntax
+        /// </summary>
+        /// <returns></returns>
+        public List<BASE_USER> GetAllCityByLINQQuery() {
+            var query = dbSet;
+            return query.Where(i => i.State == "1").OrderByDescending(i => i.ID).ToList();
+        }
+       
+    }
+}
